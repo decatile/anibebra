630be674548cd55badc5077a7b46932bb944b70d
noki3138 <nikitaserov912@gmail.com>
2023-03-27 23:00:41 +0300

    added some idk what

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+use reqwest::{Response, Url};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let resp = search_titles("nagatoro".into()).await.unwrap();
+    println!("{}", resp.text().await.unwrap());
+}
+
+async fn search_titles(q: String) -> reqwest::Result<Response> {
+    let url = Url::parse_with_params("http://api.anilibria.tv/v2/searchTitles", [("search", q)]).unwrap();
+    let resp = reqwest::get(url).await?;
+    Ok(resp)
 }
