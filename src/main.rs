92b8eda4d6f804b2c2c573a24d05d73763823db9
noki3138 <nikitaserov912@gmail.com>
2023-04-02 01:49:32 +0300

    added ui base

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
-mod api;
+use std::sync::Arc;
 
-use api::anilibria;
+use config::save_on_sigint;
+use tokio::sync::Mutex;
+use ui::ui;
+
+use crate::config::Config;
+
+mod api;
+mod config;
+mod ui;
 
 #[tokio::main]
 async fn main() {
-    let _resp = anilibria::search_titles(
-        anilibria::SearchRequestBuilder::default()
-            .search(vec!["nagatoro".into()])
-            .build()
-            .unwrap(),
-    )
-    .await
-    .unwrap();
+    let conf = Arc::new(Mutex::new(config::load().unwrap_or_else(|x| {
+        println!("Cannot use config file: {x}\nUsing empty defaults.");
+        Config::default()
+    })));
+    let conf_ref = conf.clone();
+    tokio::spawn(async move {
+        if let Err(why) = save_on_sigint(conf_ref).await {
+            println!("Cannot save to config file: {why}")
+        }
+    });
+    ui(conf.clone()).await
 }
